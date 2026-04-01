146a04117a2782c40b38a121259620b9d5afba7c
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-01 16:20:00 +0300

    chore: developing/improving methods

--- src/OOStruct/parseStateEntryToSnapshotEntry/index.ts
+++ src/OOStruct/parseStateEntryToSnapshotEntry/index.ts
@@ -8,7 +8,7 @@ export function parseStateEntryToSnapshotEntry<K>(
 ): OOStructSnapshotEntry<K> {
   return {
     __uuidv7: stateEntry.__uuidv7,
-    __value: stateEntry.__value,
+    __value: structuredClone(stateEntry.__value),
     __after: stateEntry.__after,
     __overwrites: Array.from(stateEntry.__overwrites),
   }
