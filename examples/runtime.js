f5136fdd31e298664d6f3284a9f7884bb7c99b4c
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-12 17:57:27 +0300

    chore: spec work.

---
+++ examples/runtime.js
@@ -0,0 +1,26 @@
+const example = {
+  defaults: {
+    givenName: '',
+    familyName: '',
+  },
+  entries: {
+    givenName: {
+      uuidv7: '019d81fd-a1e9-76dd-aaf0-f4dd2ac2accc',
+      value: 'Jori',
+      predecessor: '019d81fd-a1e8-72a8-bfc3-a4b6160544fc',
+      tombstones: new Set([
+        '019d81fd-a1e8-72a8-bfc3-a08e44d1211e',
+        '019d81fd-a1e8-72a8-bfc3-a4b6160544fc',
+      ]),
+    },
+    familyName: {
+      uuidv7: '019d81fd-a1ea-75cf-b513-f35976cefc93',
+      value: 'Lehtinen',
+      predecessor: '019d81fd-a1e9-76dd-aaf0-f2cdabfac485',
+      tombstones: new Set([
+        '019d81fd-a1e8-72a8-bfc3-a8082418f62a',
+        '019d81fd-a1e9-76dd-aaf0-f2cdabfac485',
+      ]),
+    },
+  },
+}
